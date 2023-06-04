a2ae0e7c86bd43726438a7cf350e9c5431bc12a8
jisoo <doh_0112@naver.com>
2023-06-04 15:50:40 +0900

    회원가입, 로그인, 로그아웃 api 통신 코드 생성

---
+++ src/api/signUp.ts
@@ -0,0 +1,135 @@
+import axios from "axios";
+import { MutationFunction } from "react-query";
+import { User } from "../types/userType";
+import { ApiResponse, ErrorResponse } from "../types/apiType";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_URL,
+});
+
+// 에러 콘솔 출력 함수
+export function showError(error: any) {
+  console.log("여기서 error 발생", error);
+}
+
+// 쿠키 가져오는 함수
+export const getCookie = (name: string) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const lastPart = parts.pop();
+    if (lastPart) {
+      return lastPart.split(";").shift();
+    }
+  }
+  return null;
+};
+
+// 액세스 토큰 가져오는 함수
+export const getAccessToken = () => {
+  return getCookie("accessToken");
+};
+
+// 리프레시 토큰 가져오는 함수
+export const getRefreshToken = () => {
+  return getCookie("refreshToken");
+};
+
+// 쿠키에 토큰 저장 함수
+export const setCookie = (name: string, value: string, days: number) => {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = `; expires=${date.toUTCString()}`;
+  }
+  document.cookie = `${name}=${value || ""}${expires}; path=/`;
+};
+
+// 회원가입 - 아이디 중복검사
+export const fetchCheckId = async (id: string) => {
+  try {
+    const response = await axiosInstance.post("/api/user/checkId", {
+      userId: id,
+    });
+    if (response.data.message === "아이디 중복 검사 성공") {
+      return response.data.message;
+    }
+    return "아이디 중복 검사 실패";
+  } catch (err: any) {
+    const errMessage = err.response.data.detail;
+    return errMessage;
+  }
+};
+
+// 회원가입 - 닉네임 중복검사
+export const fetchCheckNickname = async (nickname: string) => {
+  try {
+    const response = await axiosInstance.post("/api/user/checkNickname", {
+      nickname,
+    });
+    if (response.data.message === "닉네임 중복 검사 성공") {
+      return response.data.message;
+    }
+    return "닉네임 중복 검사 실패";
+  } catch (err: any) {
+    const errMessage = err.response.data.detail;
+    return errMessage;
+  }
+};
+
+// 회원가입
+export const fetchSignUp = async (user: User) => {
+  try {
+    const response = await axiosInstance.post("/api/user/signup", user);
+    return response.data;
+  } catch (err) {
+    showError(err);
+    throw err;
+  }
+};
+
+// 로그인
+export const fetchLogin = async (user: {
+  userId: string;
+  password: string;
+}) => {
+  try {
+    const response = await axiosInstance.post("/api/user/login", user);
+    if (response.data.message === "로그인 성공") {
+      const accessToken = response.headers.access_key;
+      const refreshToken = response.headers.refresh_key;
+
+      if (accessToken && refreshToken) {
+        setCookie("accessToken", accessToken, 1);
+        setCookie("refreshToken", refreshToken, 30);
+      }
+    }
+    return response;
+  } catch (err: any) {
+    const errMessage = err.response.data.detail;
+    return errMessage;
+  }
+};
+
+// 로그아웃
+export const fetchLogout = async () => {
+  try {
+    const accessToken = getAccessToken();
+    console.log(`ACCESS_KEY: ${accessToken}`);
+    const response = await axiosInstance.delete("/api/user/logout", {
+      headers: {
+        ACCESS_KEY: accessToken,
+      },
+    });
+    document.cookie =
+      "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    console.log(document.cookie);
+    return response;
+  } catch (err) {
+    showError(err);
+    throw err;
+  }
+};
