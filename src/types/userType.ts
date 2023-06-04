3e46d426720c25e3582e0401eb7b6ee84802a207
jisoo <doh_0112@naver.com>
2023-06-04 15:56:53 +0900

    사용자 타입 인터페이스 생성

---
+++ src/types/userType.ts
@@ -0,0 +1,10 @@
+// 사용자 타입 인터페이스
+export interface User {
+  userId: string;
+  password: string;
+  gender: string;
+  ageRange: string;
+  nickname: string;
+  agreeForGender: boolean;
+  agreeForAge: boolean;
+}
