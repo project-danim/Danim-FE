1d9b7d8bbd22f9d1792c91af27f51c89d874dfa2
jisoo <doh_0112@naver.com>
2023-06-05 17:28:51 +0900

    소셜 회원가입 추가정보 컴포넌트 추가

--- src/components/LoginPage/Redirection.tsx
+++ src/components/LoginPage/Redirection.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from "react";
-import axios, { AxiosResponse } from "axios";
 import { useNavigate } from "react-router-dom";
-import { setCookie, showError } from "../../api/signUp";
+import { axiosInstance, setCookie, showError } from "../../api/signUp";
+import SignUpForSocial from "../SignUpPage/SignUpForSocial";
 
 export const fetchKakaoToken = async (code: string) => {
-  const response = await axios.get(
-    `${import.meta.env.VITE_APP_URL}}/api/user/kakao/callback?code=${code}`
+  const response = await axiosInstance.get(
+    `/api/user/kakao/callback?code=${code}`
   );
-
   return response;
 };
 
@@ -15,28 +13,38 @@ function Redirection() {
   const url = new URL(window.location.href);
   const userCode: string | null = url.searchParams.get("code");
   const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log("userCode changed: ", userCode); // Add this line to track userCode changes
-    const fetchKakaoLogin = async () => {
-      if (userCode !== null) {
-        // userCode가 null이 아닐 때만 실행
-        try {
-          const response = await fetchKakaoToken(userCode);
-          const accessToken = response.headers.access_key;
-          const refreshToken = response.headers.refresh_key;
-          setCookie("accessToken", accessToken, 1);
-          setCookie("refreshToken", refreshToken, 30);
-          navigate("/");
-        } catch (err) {
-          showError(err);
-        }
+  const fetchKakaoLogin = async () => {
+    if (userCode !== null) {
+      // userCode가 null이 아닐 때만 실행
+      try {
+        const response = await fetchKakaoToken(userCode);
+        const accessToken = response.headers.access_key;
+        const refreshToken = response.headers.refresh_key;
+        setCookie("accessToken", accessToken, 1);
+        setCookie("refreshToken", refreshToken, 30);
+        navigate("/signup/social");
+        const userId = response.data;
+        return userId;
+      } catch (err) {
+        showError(err);
       }
-    };
-    fetchKakaoLogin();
-  }, [userCode]);
+    } else {
+      const userId = null;
+      return userId;
+    }
+    return null;
+  };
+  const userId = fetchKakaoLogin();
 
-  return <div>카카오 로그인 중~</div>;
+  return (
+    <div>
+      {userCode ? (
+        <SignUpForSocial userId={userId} />
+      ) : (
+        <>카카오 로그인 중입니다.</>
+      )}
+    </div>
+  );
 }
 
 export default Redirection;
