ea70a1f2fc3898823e1d162099a6c340c8855377
Lee YoungEun <77380372+Leezero3@users.noreply.github.com>
2023-06-12 16:57:13 +0900

    Style : create post page / Feat: Map marker pin

--- src/components/createPostPage2/ScheduleMap.tsx
+++ src/components/createPostPage2/ScheduleMap.tsx
@@ -1,13 +1,16 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import React, { useEffect, useState } from "react";
-import { Map, MapMarker, Polyline } from "react-kakao-maps-sdk";
+import {
+  Map,
+  MapMarker,
+  Polyline,
+  CustomOverlayMap,
+} from "react-kakao-maps-sdk";
 import { useRecoilState } from "recoil";
 import { selectedInfosState } from "../../recoil/post/postCreateState";
-import CommonInput from "../common/CommonInput";
+import { CommonButton, CommonInput } from "../common";
 import * as Styled from "./ScheduleMapStyle";
-import CommonButton from "../common/CommonButton";
-import PinIcon from "../Test/PinIcon";
 
 // DatePicker 스타일링 - Start
 const CustomDateInput = React.forwardRef(({ value, onClick }, ref) => (
@@ -31,6 +34,8 @@ function ScheduleMap() {
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
   // 검색된 마커 중 선택한 마커의 정보 state
   const [info, setInfo] = useState<MarkerType | null>(null);
+  // 💛update💛 지도 주소 받아오기
+
   // 검색시 지도에 표시되는 마커들의 정보 state
   const [markers, setMarkers] = useState<MarkerType[]>([]);
   // 사용자가 input에 장소를 입력한 state
@@ -47,8 +52,8 @@ function ScheduleMap() {
   // 지도 검색과 그 마커들을 표시
   const searchPlaces = () => {
     if (!map || !searchTerm) return; // searchTerm 이 비어있다면 종료!
-    const ps = new kakao.maps.services.Places();
-    ps.keywordSearch(searchTerm, (data, status) => {
+    const kakaoPlace = new kakao.maps.services.Places();
+    kakaoPlace.keywordSearch(searchTerm, (data, status) => {
       if (status === kakao.maps.services.Status.OK) {
         const bounds = new kakao.maps.LatLngBounds();
         const newMarkers = data.map((item) => {
@@ -89,107 +94,156 @@ function ScheduleMap() {
   };
 
   return (
-    <div>
-      {/* 지도 */}
-      <Map
-        center={{
-          lat: 37.566826,
-          lng: 126.9786567,
-        }}
-        style={{
-          width: "98%",
-          height: "300px",
-        }}
-        level={6}
-        onCreate={setMap}
-      >
-        {/* 검색된 마커들의 정보를 표시 */}
-        {markers.map((marker) => (
-          <MapMarker
-            key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
-            position={marker.position}
-            onClick={() => setInfo(marker)}
-          >
-            {info && info.content === marker.content && (
-              <div style={{ color: "#000" }}>{marker.content}</div>
+    <Styled.Container>
+      <Styled.Wrapper>
+        {/* 지도 */}
+        <Map
+          center={{
+            lat: 37.566826,
+            lng: 126.9786567,
+          }}
+          style={{
+            width: "100%",
+            height: "300px",
+          }}
+          level={6}
+          onCreate={setMap}
+        >
+          {/* 검색된 마커들의 정보를 표시 */}
+          {markers.map((marker) => (
+            <MapMarker
+              key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+              position={marker.position}
+              onClick={() => setInfo(marker)}
+            >
+              {info && info.content === marker.content && (
+                <div style={{ color: "#000" }}>{marker.content}</div>
+              )}
+            </MapMarker>
+          ))}
+
+          {/* 선택된 장소 연결선 */}
+          <Polyline
+            path={selectedMapInfos.map(
+              (selectedInfo) => selectedInfo.info.position
             )}
-          </MapMarker>
-        ))}
+            strokeWeight={3} // 선의 두께
+            strokeColor="black" // 선의 색깔
+            strokeOpacity={0.5} // 선의 불투명도 ( 0 - 1 / 투명 -> 불투명)
+            strokeStyle="solid" // 선의 스타일
+          />
 
-        {/* 선택된 장소 연결선 */}
-        <Polyline
-          path={selectedMapInfos.map(
-            (selectedInfo) => selectedInfo.info.position
-          )}
-          strokeWeight={3} // 선의 두께
-          strokeColor="black" // 선의 색깔
-          strokeOpacity={0.5} // 선의 불투명도 ( 0 - 1 / 투명 -> 불투명)
-          strokeStyle="solid" // 선의 스타일
-        />
-
-        {/* 마커들의 정보를 표시 */}
-        {selectedMapInfos.map((selectedInfo, index) => (
-          <MapMarker
-            key={`marker-${selectedInfo.info.content}-${selectedInfo.info.position.lat},
+          {/* 선택된 마커들의 정보를 표시 - 커스텀오버레이 (svg 형식) */}
+          {selectedMapInfos.map((selectedInfo, index) => (
+            <CustomOverlayMap
+              key={`marker-${selectedInfo.info.content}-${selectedInfo.info.position.lat},
             ${selectedInfo.info.position.lng}`}
-            position={selectedInfo.info.position}
-            onClick={() => setInfo(selectedInfo.info)}
-          >
-            {/* <div style={{ color: "#000" }}>{index + 1}</div> */}
-          </MapMarker>
-        ))}
-      </Map>
+              position={selectedInfo.info.position}
+              onClick={() => setInfo(selectedInfo.info)}
+            >
+              {/* 지도 pin - svg */}
+              <svg
+                width="41"
+                height="41"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {/* 지도 pin - svg */}
+                <path
+                  d="M12 0C7.31 0 3.5 3.81 3.5 8.5C3.5 14.28 12 24 12 
+                24C12 24 20.5 14.28 20.5 8.5C20.5 3.81 16.69 0 12 
+                0ZM12 12.75C10.83 12.75 9.875 11.795 9.875 10.625C9.875 9.455 10.83 8.5 12 
+                8.5C13.17 8.5 14.125 9.455 14.125 10.625C14.125 11.795 13.17 12.75 12 12.75Z"
+                  fill="#248CFA"
+                  stroke="#1c72ce"
+                  strokeWidth="0.5"
+                />
+                <circle
+                  cx="12"
+                  cy="9"
+                  r="5.5"
+                  fill="#fbfbfb"
+                  stroke="#1c72ce"
+                  strokeWidth="0.5"
+                />
+                <text
+                  x="11.7"
+                  y="12.5"
+                  fill="#000000"
+                  textAnchor="middle"
+                  fontSize="9"
+                >
+                  {index + 1}
+                </text>
+              </svg>
+            </CustomOverlayMap>
+          ))}
+        </Map>
+        {/* 검색 input */}
+        <Styled.InputAddWrapper>
+          <CommonInput
+            type="text"
+            placeholder="장소를 추가해주세요."
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />
+        </Styled.InputAddWrapper>
 
-      <PinIcon />
+        {/* 날짜 선택 캘린더 */}
+        <Styled.InputAddWrapper>
+          <DatePicker
+            selected={selectedDate}
+            onChange={(date: Date | null) => setSelectedDate(date)}
+            minDate={today} // 오늘 날짜를 포함한 그 이후 날짜만 선택 가능
+            placeholderText="출발 날짜를 알려주세요."
+            customInput={<CustomDateInput />}
+          />
+        </Styled.InputAddWrapper>
 
-      {/* 검색 input */}
-      <CommonInput
-        type="text"
-        placeholder="장소를 추가해주세요."
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
-
-      {/* 날짜 선택 캘린더 */}
-      <div>
-        <DatePicker
-          selected={selectedDate}
-          onChange={(date: Date | null) => setSelectedDate(date)}
-          minDate={today} // 오늘 날짜를 포함한 그 이후 날짜만 선택 가능
-          placeholderText="출발 날짜를 알려주세요."
-          customInput={<CustomDateInput />}
-        />
-      </div>
-
-      {/* 선택 정보 저장 버튼 */}
-      <CommonButton type="button" onClick={addSelectedInfo}>
-        + 등록하기
-      </CommonButton>
-
-      {/* 선택한 정보를 화면에 표시 */}
-      {selectedMapInfos.map((selectedInfo, index) => (
-        <div key={selectedInfo.id}>
-          <span>{index + 1}</span>
-          <div>{selectedInfo.info.content}</div>
-          {selectedInfo.date &&
-            `${selectedInfo.date.toLocaleDateString()} (${
-              days[selectedInfo.date.getDay()]
-            })`}
-          <button
-            type="button"
-            onClick={() => {
-              setSelectedMapInfos(
-                selectedMapInfos.filter(
-                  (makerInfo) => makerInfo.id !== selectedInfo.id
-                )
-              );
-            }}
-          >
-            x
-          </button>
-        </div>
-      ))}
-    </div>
+        {/* 선택 정보 저장 버튼 */}
+        <Styled.InputAddWrapper>
+          <CommonButton type="button" onClick={addSelectedInfo}>
+            + 등록하기
+          </CommonButton>
+        </Styled.InputAddWrapper>
+        <Styled.MapInfoContainer>
+          {/* 선택한 정보를 화면에 표시 */}
+          {selectedMapInfos.map((selectedInfo, index) => (
+            <Styled.MapInfoWrapper key={selectedInfo.id}>
+              {/* 넘버링 */}
+              <Styled.CircleNumbering>{index + 1}</Styled.CircleNumbering>
+              {/* 지도 정보 */}
+              <Styled.MapInfoTextWrapper>
+                <Styled.MapTextWrapper>
+                  <Styled.MapInfoText>
+                    {selectedInfo.info.content}
+                  </Styled.MapInfoText>
+                  <Styled.MapDateText>
+                    {selectedInfo.date &&
+                      `${selectedInfo.date.toLocaleDateString()} (${
+                        days[selectedInfo.date.getDay()]
+                      })`}
+                  </Styled.MapDateText>
+                </Styled.MapTextWrapper>
+                <Styled.MapInfoDeleteButton
+                  type="button"
+                  onClick={() => {
+                    setSelectedMapInfos(
+                      selectedMapInfos.filter(
+                        (makerInfo) => makerInfo.id !== selectedInfo.id
+                      )
+                    );
+                  }}
+                >
+                  x
+                </Styled.MapInfoDeleteButton>
+              </Styled.MapInfoTextWrapper>
+            </Styled.MapInfoWrapper>
+          ))}
+        </Styled.MapInfoContainer>
+      </Styled.Wrapper>
+    </Styled.Container>
   );
 }
 
--- src/components/createPostPage2/ScheduleMapStyle.tsx
+++ src/components/createPostPage2/ScheduleMapStyle.tsx
@@ -1,13 +1,128 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  background-color: #ececec;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 97%;
+  margin: 10px 0px;
+`;
+
 export const StyledInput = styled.div`
   border: 0.5px solid #a3a3a3;
   font-size: 16px;
   box-sizing: border-box;
-  padding: 9.5px 0;
+  padding: 10.5px 0;
   padding-left: 12px;
   width: 100%;
   line-height: 22px;
   border-radius: 8px;
+  background-color: #ffffff;
   color: #a3a3a3;
+`;
+
+// 지도 정보 Container
+export const MapInfoContainer = styled.div`
+  width: 100%;
+  height: 450px;
+  margin-bottom: 30px;
+  border-top: solid 1px black;
+  overflow: auto; /* 스크롤 바 스타일링 */
+  overflow-x: hidden;
+
+  ::-webkit-scrollbar {
+    width: 16px; /* 스크롤 바 너비 */
+    background-color: #f2f2f2; /* 스크롤 바 배경색 */
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #a4bf3b; /* 스크롤 바 색상 */
+    border-radius: 4px; /* 스크롤 바 모서리 둥글기 */
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #2f5901; /* 스크롤 바 호버 시 색상 */
+  }
+`;
+
+// 지도 정보 Wrapper
+export const MapInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 95%;
+  margin: 32px 5px;
+`;
+
+// input - add wrapper.
+export const InputAddWrapper = styled.div`
+  width: 100%;
+  margin: 10px 0 10px 0;
+`;
+
+// 원형 숫자
+export const CircleNumbering = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #2e5902;
+  color: #ffffff;
+  text-align: center;
+  margin-right: 8px;
+  margin-bottom: 3px;
+  font-size: 16px;
+  font-family: "Pridi", serif;
+`;
+
+// 지도 텍스트 wrapper
+export const MapTextWrapper = styled.div`
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+// 방문 장소
+export const MapInfoText = styled.div`
+  font-size: 16px;
+`;
+
+// 방문 날짜
+export const MapDateText = styled.div`
+  font-size: 12px;
+  color: #c5c5c5;
+`;
+
+// TextWrapper
+export const MapInfoTextWrapper = styled.div`
+  border: solid 1px #e7e7e7;
+  border-radius: 5px;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+  background-color: white;
+  height: 120px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+// 지도 정보 삭제 버튼
+export const MapInfoDeleteButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 12.5px;
+  background-color: #f2f2f2;
+  border: none;
+  border-radius: 7px;
+  padding: 10px 12px;
+  font-size: 16px;
+  height: 30px;
+  cursor: pointer;
 `;
