e3d9b7f4da54ee2bbfb1d4a30b9b859fceb118fe
jisoo <doh_0112@naver.com>
2023-06-15 02:02:12 +0900

    컴포넌트의 디스플레이 네임 설정 추가

--- src/components/HomePage/Post.tsx
+++ src/components/HomePage/Post.tsx
@@ -1,82 +1,70 @@
-import React, { useEffect } from "react";
-import { useRecoilState } from "recoil";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  filteredAge,
-  filteredGroupSize,
-  filteredLocation,
-  isSearchClicked,
-} from "../../recoil/filter/filterdPost";
 import { PostProps } from "../../types/postType";
+import st from "./PostST";
 
-const Post = React.forwardRef<any, PostProps>(
-  ({ post, allFilterList }, ref) => {
-    // 네비게이션 함수 생성
-    const navigate = useNavigate();
+const Post = React.forwardRef<any, PostProps>(({ post }, ref) => {
+  // ({ post, allFilterList }, ref) => {
+  // 네비게이션 함수
+  const navigate = useNavigate();
 
-    // 지역, 인원수, 연령대 선택값 state
-    const [selectedLocation, setSelectedLocation] =
-      useRecoilState(filteredLocation);
-    const [selectedGroupSize, setSelectedGroupSize] =
-      useRecoilState(filteredGroupSize);
-    const [selectedAge, setSelectedAge] = useRecoilState(filteredAge);
-    // 검색 토글 state
-    const [isSearched, handleSearchClicked] = useRecoilState(isSearchClicked);
+  // 자세히 보기 버튼 클릭시
+  const handleDetailButtonClick = (id: number) => {
+    navigate(`/post/${id}`);
+  };
 
-    // 자세히 보기 버튼 클릭시
-    const handleDetailButtonClick = (id: number) => {
-      navigate(`/post/${id}`);
-    };
+  // 날짜 변환 함수
+  const getEndDate = (endDate: number) => {
+    const date = new Date(endDate);
+    const year = `${date.getFullYear()}`.slice(-2);
+    // 한 자리일 경우 0 붙여서 두 자리로 자름
+    const month = `0${date.getMonth() + 1}`.slice(-2);
+    const day = `0${date.getDate()}`.slice(-2);
+    const tripEndDate = `${year}. ${month}. ${day}`;
+    return tripEndDate;
+  };
 
-    // 날짜 변환 함수
-    const getEndDate = (endDate: number) => {
-      const date = new Date(endDate);
-      const year = `${date.getFullYear()}`.slice(-2);
-      // 한 자리일 경우 0 붙여서 두 자리로 자름
-      const month = `0${date.getMonth() + 1}`.slice(-2);
-      const day = `0${date.getDate()}`.slice(-2);
-      const tripEndDate = `${year}-${month}-${day}`;
-      return tripEndDate;
-    };
-    return (
-      <div>
-        {/* {isSearched ? ( */}
-        <div key={post.id} ref={ref}>
-          <p>게시글 제목 : {post.title}</p>
-          <p>닉네임 : {post.nickname}</p>
-          <img src={post.imageUrl} alt="게시글 이미지" />
+  // 연령대 문자열 배열로 변환 함수
+  const getAgeKeyword = (ages: string) => {
+    const newAges = ages.split(",");
+    return newAges;
+  };
+
+  return (
+    <st.expiredPostContainer expired={post.isRecruitmentEnd}>
+      <st.postContainer key={post.id} ref={ref}>
+        <st.postTitle>{post.title}</st.postTitle>
+        <st.postNickname>{post.nickname}</st.postNickname>
+        <st.postImage src={post.imageUrl} alt="게시글 이미지" />
+        <st.groupSizeAndDateContainer>
           <p>
             모집 인원 : {post.numberOfParticipants}/{post.groupSize}
           </p>
           <p>모집 기한 : {getEndDate(post.recruitmentEndDate)}</p>
-          <div>
-            게시글 키워드 :
-            <span>{`${post.keyword} ${post.location} ${post.groupSize}명 ${post.ageRange}`}</span>
-          </div>
-          <button
-            type="button"
-            onClick={() => handleDetailButtonClick(post.id)}
-          >
-            자세히 보기
-          </button>
-        </div>
-        {/* ) : (
-          <div key={post.id} ref={ref}>
-            <p>게시글 제목 : {post.title}</p>
-            <p>닉네임 : {post.nickname}</p>
-            <img src={post.imageUrl} alt="게시글 이미지" />
-            <p>
-              모집 인원 : {post.numberOfParticipants}/{post.groupSize}
-            </p>
-            <p>모집 기한 : {getEndDate(post.recruitmentEndDate)}</p>
-            <div>게시글 지역 : {post.location}</div>
-            <div>게시글 연령대 : {post.ageRange}</div>
-            <button type="button">자세히 보기</button>
-          </div>
-        )} */}
-      </div>
-    );
-  }
-);
+        </st.groupSizeAndDateContainer>
+        <st.keywordContainer>
+          <st.CommonKeywordButton>{post.keyword}</st.CommonKeywordButton>
+          <st.CommonKeywordButton>{post.location}</st.CommonKeywordButton>
+          <st.CommonKeywordButton>{post.groupSize}명</st.CommonKeywordButton>
+          {getAgeKeyword(post.ageRange).map((age) => (
+            <div key={age}>
+              <st.CommonKeywordButton>{age}</st.CommonKeywordButton>
+            </div>
+          ))}
+        </st.keywordContainer>
+        <st.CommonButton
+          buttonName="postDetail"
+          type="button"
+          onClick={() => handleDetailButtonClick(post.id)}
+        >
+          자세히 보기
+        </st.CommonButton>
+      </st.postContainer>
+    </st.expiredPostContainer>
+  );
+});
+
+// React.forwardRef와 같이 고차 컴포넌트(HOC)를 사용하는 경우, 자동으로 컴포넌트 이름을 추정할 수 없으므로, displayName을 직접 설정하는 것이 좋다.
 Post.displayName = "Post";
+
 export default Post;
