2538fab375f347181349ae4b2e294e61f48ee83b
Lee YoungEun <77380372+Leezero3@users.noreply.github.com>
2023-06-16 17:46:39 +0900

    Feat : 로그인시 session Storage 값 추가 (nickname, profileUrl)

--- src/components/LoginPage/Login.tsx
+++ src/components/LoginPage/Login.tsx
@@ -44,7 +44,11 @@ function Login() {
       }
       if (response.data.message === "로그인 성공") {
         const { id } = response.data.data;
+        const { nickName: nickname } = response.data.data;
+        const { myPageImageUrl: profileUrl } = response.data.data;
         sessionStorage.setItem("id", id);
+        sessionStorage.setItem("nickname", nickname);
+        sessionStorage.setItem("profileUrl", profileUrl);
         return navigate("/");
       }
       return alert("로그인을 다시 시도해주세요.");
