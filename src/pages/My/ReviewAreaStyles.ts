146617489850716c7a8327cfbb0e78fa4fc076e2
nevertheless1031 <124845741+nevertheless1031@users.noreply.github.com>
2023-06-19 05:46:52 +0900

    api 연결 및 css 수정 중

--- src/pages/My/ReviewAreaStyles.ts
+++ src/pages/My/ReviewAreaStyles.ts
@@ -1,84 +1,90 @@
 import styled from "styled-components";
 
-
-// 최상단 컨테이너
-export const ParentContainer = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
 // 프로필 박스 영역
 export const ProfileArea = styled.div`
   width: 1120px;
   height: 315px;
   display: flex;
+  justify-content: center; 
   background-color: white;
+
 `;
 
 // 프로필 박스 내 이미지 박스 영역
 export const ImageBox = styled.div`
-  width: 170px;
-  height: 170px;
+  width: 232px;
+  height: 232px;
 `;
 
 // 프로필 박스 내 이미지 업로드 영역(원, 클릭시~)
 export const ImageArea = styled.div`
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
+  width: 232px;
+  height: 232px;
+  border-radius: 200px;
   background-color: rgba(181, 191, 105, 1);
-  margin-top: 30px;
-`;
-
-// UserInfo 컴포넌트를 감싸는 컨테이너
-export const UserInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
 `;
 
 // 유저인포(스코어, 닉네임, 수정)영역
 export const UserInfo = styled.div`
   display: flex;
-  align-items: flex-start;
-  width: 700px;
-  height: 20px;
-  padding: 10px;
-  justify-content: space-between;
+  width: 1120px;
+  height: 42px;
 `;
 
-// 유저 스코어 영역
-export const UserScore = styled.div`
-  border: 1px solid black;
-  width: 120px;
-  height: 22px;
-  border-radius: 7px;
-  border: none;
-  margin-right: 30px;
+// 유저 스코어 영역(mile 상위태그)
+export const ProfileMileContainer = styled.div`
+  width: 79px;
+  height: 33px;
+  border: 1px solid rgba(243, 246, 250, 1);
+  border-radius: 20px;
+  background-color: rgba(243, 246, 250, 1);
+  display: flex;            
+  justify-content: center; 
+  align-items: center;   
+  font-size: 14px;
+  line-height: 17px;
+
+`;
+// 마일(review.point) 노출 영역
+export const ProfileMileBox = styled.div`
+  width: 34px;
+  height: 21px;
+  border: 1px solid rgba(126, 168, 227, 1);
+  border-radius: 6px;
+  background-color: rgba(126, 168, 227, 1);
+  color: rgba(255, 255, 255, 1);
+  margin-left: 4px;
+  
+`;
+// mile 텍스트가 들어간 파란색 박스 영역
+export const ProfileMile = styled.div`
+text-align: center;
+margin-top: 2px;
 `;
 
 // 유저 닉네임 영역
-export const NickName = styled.div`
-  width: 120px;
-  height: 22px;
-  border-radius: 7px;
-  border: none;
+
+export const UserNickName = styled.div`
+  width: 70px;
+  height: 24px;
+  font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+margin-left: 8px;
+margin-top: 2px;
+
 `;
 
 // 수정 버튼 영역
 export const PixButton = styled.button`
-  width: 141px;
+  width: 107px;
   height: 42px;
-  border: 1px solid #d5d5d5;
+  border: 1px solid rgba(213, 213, 213, 1);
   border-radius: 8px;
   background-color: white;
-  color: black;
-  position: relative;
-  align-self: flex-end;
-  margin-top: 10px;
-  margin-left: 400px;
+  color: rgba(133, 133, 133, 1);
+  margin-left: 632px;
 `;
 
 // 텍스트 입력 영역
@@ -91,12 +97,22 @@ export const TextArea = styled.textarea`
   font-size: 15px;
   /* margin-bottom: 100px; */
   padding: 20px;
-  gap: 10px;
   border: 1px solid #d6d6d6;
-  box-shadow: 0px 0px 0px #cbdafc;
   border-radius: 5px;
   resize: none;
 `;
+
+
+
+
+
+export const ReviewMile = styled.div`
+  width: 75px;
+  height: 14px;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+
 
 // 후기, 게시글 영역
 export const PostContainer = styled.div`
@@ -146,12 +162,6 @@ export const CreatedTime = styled.div`
   color: rgba(133, 133, 133, 1);
   width: 74px;
   height: 34px;
-`;
-
-export const Mile = styled.div`
-  width: 75px;
-  height: 14px;
-  color: rgba(0, 0, 0, 0.5);
 `;
 
 export const ReviewNickName = styled.div`
