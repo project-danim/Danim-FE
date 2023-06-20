8da099a5f08a6b64ab3cae1db13484832e5991b2
jisoo <doh_0112@naver.com>
2023-06-20 09:08:59 +0900

    글로벌 스타일 폰트 추가

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,14 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  /* reset.css */
+  /* pretendard 폰트 설정 */
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+  /* reset.css */ 
   body {
     margin: 0;
     padding: 0;
-  }
-  p {
-      margin: 0;
-      padding : 0;
+    font-weight: 400;
+    > * {
+      font-family: 'Pretendard-Regular','Roboto', 'Noto Sans KR', sans-serif;
     }
+  }
+  p,ul,li {
+    padding: 0;
+    margin: 0;
+  } 
 `;
 export default GlobalStyle;
