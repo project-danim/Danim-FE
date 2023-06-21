3f908ec800e0f022399029fe64dc43c3180b948f
jisoo <doh_0112@naver.com>
2023-06-22 04:35:42 +0900

    홈페이지 배너 이미지 컴포넌트 연결

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,38 +1,6 @@
-import styled from "styled-components";
 import { useEffect } from "react";
 import Main from "../components/HomePage/Main";
-
-const HomeBanner = styled.div`
-  width: 100%;
-  min-height: 340px;
-  max-height: 340px;
-  background-image: url("main/danimBanner.svg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-color: #f9f6e3;
-  // 노트북
-  @media (max-width: 1500px) and (min-width: 1025px) {
-    min-height: 270px;
-  }
-  // 태블릿
-  @media (max-width: 1024px) and (min-width: 940px) {
-    background-image: url("main/danimBanner_tablet.svg");
-    min-height: 230px;
-    background-size: cover;
-  }
-  @media (max-width: 941px) and (min-width: 391px) {
-    background-image: url("main/danimBanner_tablet.svg");
-    min-height: 230px;
-    background-size: contain;
-  }
-  // 모바일
-  @media (max-width: 390px) {
-    background-image: url("main/danimBanner_mobile.svg");
-    border-radius: 6px;
-    min-height: 115px;
-  }
-`;
+import BannerImg from "../components/HomePage/BannerImg";
 
 function HomePage() {
   const openPopup = () => {
@@ -67,7 +35,7 @@ function HomePage() {
 
   return (
     <>
-      <HomeBanner />
+      <BannerImg />
       <Main />
     </>
   );
