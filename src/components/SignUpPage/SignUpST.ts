65c34e6612d9e8ad9666ff24187c8a515a93a1f9
jisoo <doh_0112@naver.com>
2023-06-23 04:34:42 +0900

    버튼 스타일링 변경

--- src/components/SignUpPage/SignUpST.ts
+++ src/components/SignUpPage/SignUpST.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 // 공통 사용 버튼 프롭 타입
 type OriginalButtonProps = {
@@ -12,18 +12,15 @@ type FormExplainTextProps = {
 
 // 공통 사용 버튼
 const OriginalButton = styled.button<OriginalButtonProps>`
-  font-size: 14px;
-  line-height: 22px;
   padding: 10px 17.5px;
   width: 100%;
-  color: #ffffff;
-  background-color: ${({ buttonName }) =>
-    buttonName === "signUp" ? "#2E5902" : "#A3BF3B"};
-  background-color: ${(props) => (props.active ? "#2E5902" : "#a3bf3b")};
   border: none;
   border-radius: 8px;
-  font-weight: 400;
-  font-family: "Pretendard-Regular";
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 22px;
+  font-family: inherit;
   cursor: pointer;
   &:hover {
     box-shadow: 0px 2px 2px 0px #0000001a;
@@ -31,11 +28,35 @@ const OriginalButton = styled.button<OriginalButtonProps>`
     border: 2px solid #eaedd4;
     padding: 8px 15.5px;
   }
-  @media (max-width: 1400px) and (min-width: 320px) {
-    font-size: 12px;
-    height: 32px;
-    padding: 0 13px;
-  }
+
+  ${({ buttonName }) =>
+    buttonName === "checkOverlap" &&
+    css`
+      background-color: #a3bf3b;
+      color: #fff;
+      font-weight: 400;
+      font-size: 14px;
+      padding: 10px 12px;
+    `}
+
+  ${({ buttonName }) =>
+    buttonName === "other" &&
+    css`
+      background-color: #a3bf3b;
+      color: #fff;
+    `}
+  ${(props) =>
+    props.active &&
+    css`
+      background-color: #2e5902;
+    `}
+
+  ${({ buttonName }) =>
+    buttonName === "signUp" &&
+    css`
+      background-color: #2e5902;
+      font-size: 14px;
+    `}
 `;
 
 // 공통 입력창
@@ -52,12 +73,6 @@ const CommonInput = styled.input`
   &:focus {
     outline: 1px solid #a3bf3b;
   }
-  @media (max-width: 1400px) and (min-width: 320px) {
-    font-size: 11px;
-    padding: 8px 0;
-    padding-left: 8px;
-    line-height: 15px;
-  }
 `;
 
 // 공통 입력값 에러 텍스트
@@ -69,26 +84,17 @@ const CommonErrorText = styled.div`
   margin-top: 8px;
   margin-bottom: 32px;
   padding-left: 2px;
-  @media (max-width: 1400px) and (min-width: 320px) {
-    font-size: 10px;
-  }
 `;
 
 // 공통 정보 동의 텍스트
 const CommonAgreeForInfoText = styled.span`
   font-size: 13px;
-  @media (max-width: 1400px) and (min-width: 320px) {
-    font-size: 10px;
-  }
 `;
 
 // 공통 정보 동의 체크박스
 const CommonAgreeForInfoInput = styled.input`
   position: relative;
   top: 2px;
-  @media (max-width: 1400px) and (min-width: 320px) {
-    top: 3px;
-  }
 `;
 
 const ContainerForm = styled.form`
@@ -102,10 +108,6 @@ const ContainerForm = styled.form`
   flex-direction: column;
   margin-top: 106px;
   margin-bottom: 144px;
-  @media (max-width: 1400px) and (min-width: 320px) {
-    font-size: 10px;
-    max-width: 270px;
-  }
 `;
 
 // 소셜 로그인 텍스트 ("SNS로 로그인")
@@ -136,7 +138,7 @@ const FormExplainText = styled.p<FormExplainTextProps>`
     top: 6px;
     right: 0;
   }
-  @media (max-width: 1400px) and (min-width: 320px) {
+  /* @media (max-width: 1400px) and (min-width: 320px) {
     font-size: 10px;
     &::before {
       width: ${(props) => (props.pageName === "loginPage" ? "80px" : "40px")};
@@ -148,7 +150,7 @@ const FormExplainText = styled.p<FormExplainTextProps>`
     }
     margin-bottom: 30px;
     margin-top: ${(props) => (props.pageName === "loginPage" ? "30px" : "0")};
-  }
+  } */
 `;
 
 const SignUpExplainText = styled.p`
@@ -159,9 +161,6 @@ const SignUpExplainText = styled.p`
 // 회원가입 정보 입력창 컨테이너
 const UserInfoContainer = styled.div`
   margin-top: 36px;
-  @media (max-width: 1400px) and (min-width: 320px) {
-    margin-top: 10px;
-  }
 `;
 
 // 아이디
@@ -180,27 +179,50 @@ const IdAreaExplainText = styled.p`
 
 // 성별
 const GenderLabelContainer = styled.label`
-  display: grid;
-  grid-template-rows: repeat(1, 1fr);
-  grid-template-columns: repeat(2, 1fr);
-  gap: 20px;
-  margin-bottom: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  & > button:first-child {
+    margin-right: 20px;
+  }
 `;
 
 const GenderAriaContainer = styled.div`
-  margin-bottom: 29px;
+  margin-bottom: 32px;
 `;
 
 // 연령
-const AgeAriaContainer = styled.label`
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: repeat(3, 1fr);
-  justify-content: space-between;
-  gap: 15px;
-  margin-bottom: 8px;
-  @media (max-width: 1400px) and (min-width: 320px) {
-    gap: 10px 13px;
+const AgeAriaContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    & > button {
+      width: calc(33.33% - 15px);
+      margin-bottom: 15px;
+    }
+  }
+`;
+
+// 약관 동의 컨테이너
+const AgreeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  row-gap: 14px;
+  font-size: 14px;
+  color: #868686;
+  font-family: Noto Sans KR;
+  padding: 13px 16px;
+  border-radius: 8px;
+  border: 0.5px solid #a3a3a3;
+  margin-top: -15px;
+  margin-bottom: 96px;
+  & > label > span {
+    font-size: 14px;
   }
 `;
 
@@ -219,4 +241,5 @@ export default {
   GenderLabelContainer,
   GenderAriaContainer,
   AgeAriaContainer,
+  AgreeContainer,
 };
