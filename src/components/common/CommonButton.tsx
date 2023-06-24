832f96ddf630c8ac64ef4d3f2f9d699785167f7c
Lee YoungEun <77380372+Leezero3@users.noreply.github.com>
2023-06-24 20:37:02 +0900

    Style : common button 추가

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -6,10 +6,8 @@ const CommonButton = styled.button<any>`
   padding: 10px 17.5px;
   box-sizing: border-box;
   width: 100%;
-  /* max-width: 110px; */
   color: 5C5C5C;
-  background-color: ${(props) =>
-    props.bgColor || "var(--button-6-default-color)"};
+  background-color: ${(props) => props.bgColor || props.bgColor};
   border: none;
   border-radius: 8px;
   font-weight: 400;
@@ -17,11 +15,9 @@ const CommonButton = styled.button<any>`
     cursor: pointer;
     background-color: var(--button-6-hover-color);
   }
-  /* @media (max-width: 1400px) and (min-width: 320px) {
-font-size: 11px;
-padding: 8px 11px;
-line-height: 15px;
-} */
+  &:active {
+    background-color: var(--button-6-pressed-color);
+  }
 `;
 
 export default CommonButton;
