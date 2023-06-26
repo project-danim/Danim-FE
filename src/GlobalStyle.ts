1e15d3fb2414fda8a13ec5fe6b741fe7efa55e2c
jisoo <doh_0112@naver.com>
2023-06-26 17:18:31 +0900

    reset.css 코드 제거

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,31 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  /* pretendard 폰트 설정 */
   @font-face {
     font-family: 'Pretendard';
     src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
     font-weight: 400;
     font-style: normal;
   }
-  /* reset.css */ 
+
   body {
-    margin: 0;
-    padding: 0;
+    font-family: 'Pretendard', 'Roboto', 'Noto Sans KR', sans-serif;
     font-weight: 400;
-    > * {
-      font-family: 'Pretendard','Roboto', 'Noto Sans KR', sans-serif;
-    }
   }
-  p,ul,li,h1,h2 {
-    padding: 0;
+  h1,h2,h3,h4 {
     margin: 0;
-    list-style: none;
-  } 
-  button {
-    padding: 0;
-    border : none;
   }
+
+
   // react-datepicker 다른 요소에 가려지는것 방지
   .react-datepicker-popper {
     z-index: 3 !important; // 혹시 다른 스타일에 의해 오버라이드 되는 것을 방지하기 위해 !important를 사용하였습니다.
