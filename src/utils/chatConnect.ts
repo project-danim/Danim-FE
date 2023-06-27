6634f55b3b2a7fd94d4bcc382a56121054328f4a
jisoo <doh_0112@naver.com>
2023-06-28 01:18:40 +0900

    받아온 배열 객체를 기준으로 알람 설정하게 추가

--- src/utils/chatConnect.ts
+++ src/utils/chatConnect.ts
@@ -1,9 +1,14 @@
 // 웹소켓 연결을 위한 커스텀 훅
 import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import SockJS from "sockjs-client";
 import StompJs from "stompjs";
+import hasNewChatState from "../recoil/chat/alarm";
 
 const useChatConnect = (userId: string) => {
+  // 채팅 알람 state
+  const [, setHasNewChat] = useRecoilState(hasNewChatState);
+
   useEffect(() => {
     const sock = new SockJS(`${import.meta.env.VITE_APP_URL}/ws-stomp`);
     const stomp = StompJs.over(sock);
@@ -11,9 +16,18 @@ const useChatConnect = (userId: string) => {
       {},
       () => {
         stomp.subscribe(`/sub/alarm/${userId}`, (data: any) => {
-          // console.log("여기서 확인해야함", data);
+          // 받아온 데이터가 문자열이므로 JSON 객체로 변환
+          const alarmArray = JSON.parse(data.body);
+          // console.log(alarmArray);
+          // 받아온 배열 안의 알람 객체의 value가 0이 아닌게 있으면
+          alarmArray.forEach((chatAlarm: any) => {
+            const alarm = Object.values(chatAlarm);
+            if (alarm[0] !== 0) {
+              return setHasNewChat(() => true);
+            }
+            return null;
+          });
         });
-        // console.log("구독완료");
       },
       // 에러 콜백 함수는 string 또는 Frame 타입의 파라미터를 받아야 함.
       (err: string | StompJs.Frame) => {
