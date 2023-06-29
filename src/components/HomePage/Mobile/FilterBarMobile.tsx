de0714dd4e8c0a58a6e562f117a8f7a8a8a0d2d8
jisoo <doh_0112@naver.com>
2023-06-29 15:43:39 +0900

    모바일 필터바 생성

---
+++ src/components/HomePage/Mobile/FilterBarMobile.tsx
@@ -0,0 +1,11 @@
+import FilterBar from "../FilterBar";
+
+function FilterBarMobile() {
+  return (
+    <div>
+      <p>다님 검색하기</p>
+      <FilterBar />
+    </div>
+  );
+}
+export default FilterBarMobile;
