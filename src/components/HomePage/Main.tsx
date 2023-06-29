93d5ff611ef1b5901362e3efd998cff3531f1426
jisoo <doh_0112@naver.com>
2023-06-29 15:43:31 +0900

    모바일 스타일링 수정

--- src/components/HomePage/Main.tsx
+++ src/components/HomePage/Main.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import { useState } from "react";
 import FilterBar from "./FilterBar";
 import PostList from "./PostList";
 import useChatConnect from "../../utils/chatConnect";
+import FilterBarMobile from "./Mobile/FilterBarMobile";
 
 const MainPageContainer = styled.div`
   max-width: 1120px;
@@ -9,9 +11,18 @@ const MainPageContainer = styled.div`
   margin: 0 auto;
   text-align: center;
   margin-bottom: 154px;
+  // 모바일
+  @media (min-width: 375px) and (max-width: 390px) {
+    padding: 20px;
+    box-sizing: border-box;
+    border: 2px solid red;
+  }
 `;
 
 function Main() {
+  // 모바일 픽셀인지 아닌지에 대한 상태
+  const [isMobile] = useState(window.matchMedia("(max-width: 391px)").matches);
+
   // 로컬 스토리지에 저장된 사용자 아이디
   const userId = localStorage.getItem("id");
   if (userId) {
@@ -19,10 +30,18 @@ function Main() {
   }
 
   return (
-    <MainPageContainer>
-      <FilterBar />
-      <PostList />
-    </MainPageContainer>
+    <div>
+      {isMobile ? (
+        <MainPageContainer>
+          <FilterBarMobile />
+        </MainPageContainer>
+      ) : (
+        <MainPageContainer>
+          <FilterBar />
+          <PostList />
+        </MainPageContainer>
+      )}
+    </div>
   );
 }
 
