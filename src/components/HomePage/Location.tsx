59d7bce8aaf8fe64a53e8e5714b728163d3b346f
jisoo <doh_0112@naver.com>
2023-06-30 03:58:04 +0900

    모바일 픽셀인지 확인하는 로직 추가

--- src/components/HomePage/Location.tsx
+++ src/components/HomePage/Location.tsx
@@ -1,10 +1,14 @@
 import { useRecoilState } from "recoil";
+import { useState } from "react";
 import useToggle from "../../hooks/useToggle";
 import { filteredLocation } from "../../recoil/filter/filterdPost";
 import st from "./FilterBarST";
 import { locationList } from "./FilterListData";
 
 function LocationFilter() {
+  // 모바일 픽셀인지 아닌지에 대한 상태
+  const [isMobile] = useState(window.matchMedia("(max-width: 430px)").matches);
+
   // 지역 버튼 선택 토글 state
   const [isLocationToggled, handleIsLocationToggled] = useToggle(false);
   const [selectedLocation, setSelectedLocation] =
@@ -19,9 +23,11 @@ function LocationFilter() {
   };
   return (
     <st.StyleContainer>
-      <st.CommonLableNameText>지역</st.CommonLableNameText>
+      {isMobile ? null : <st.CommonLableNameText>지역</st.CommonLableNameText>}
       <st.CommonDropDownButton type="button" onClick={handleIsLocationToggled}>
-        <st.CommonSelectedValue>{selectedLocation}</st.CommonSelectedValue>
+        <st.CommonSelectedValue>
+          {selectedLocation !== "" ? selectedLocation : "지역을 선택해주세요."}
+        </st.CommonSelectedValue>
         <st.CommonUnderButton>지역 선택하기</st.CommonUnderButton>
       </st.CommonDropDownButton>
       {isLocationToggled ? (
