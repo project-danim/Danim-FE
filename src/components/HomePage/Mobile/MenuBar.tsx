a534cd08969bc5976dabbfe194753a32dc46f51b
jisoo <doh_0112@naver.com>
2023-06-30 04:34:04 +0900

    홈, 마이페이지, 채팅 버튼 클릭시 이벤트 연결

--- src/components/HomePage/Mobile/MenuBar.tsx
+++ src/components/HomePage/Mobile/MenuBar.tsx
@@ -49,6 +49,11 @@ function MenuBar() {
     },
   });
 
+  // 홈 버튼 클릭시
+  const handleHomeButtonClick = () => {
+    navigate("/");
+  };
+
   // 회원가입 버튼 클릭시
   const handleClickSignUpButton = () => {
     navigate("/signUp");
@@ -64,10 +69,25 @@ function MenuBar() {
     mutateLogout();
   };
 
+  // 채팅하기 버튼 클릭시
+  const handleChatButtonClick = () => {
+    navigate("/chat-list");
+  };
+
+  // 마이페이지 버튼 클릭시
+  const handleMyPageButtonClick = () => {
+    navigate("/mypage");
+  };
+
   // 닫기 버튼 클릭시
   const handleCloseButtonClick = () => {
     window.history.back();
   };
+  // 동행 만들기 버튼 클릭시
+  const handleCreatePostClick = () => {
+    navigate("/create-post/step1");
+  };
+
   return (
     <st.MenuBarContainer>
       {userAccessCookie || userRefreshCookie ? (
@@ -92,11 +112,19 @@ function MenuBar() {
             <button type="button">로그아웃</button>
           </st.SignUpLoginContainer>
           <st.HomeAndMyAndChatContainer>
-            <button type="button">홈</button>
-            <button type="button">My Page</button>
-            <button type="button">채팅 목록</button>
+            <button type="button" onClick={handleHomeButtonClick}>
+              홈
+            </button>
+            <button type="button" onClick={handleMyPageButtonClick}>
+              My Page
+            </button>
+            <button type="button" onClick={handleChatButtonClick}>
+              채팅 목록
+            </button>
           </st.HomeAndMyAndChatContainer>
-          <st.PostButton type="button">동행 만들기</st.PostButton>
+          <st.PostButton type="button" onClick={handleCreatePostClick}>
+            동행 만들기
+          </st.PostButton>
         </>
       ) : (
         // 로그인 되지 않은 상태
@@ -117,11 +145,19 @@ function MenuBar() {
             </button>
           </st.SignUpLoginContainer>
           <st.HomeAndMyAndChatContainer>
-            <button type="button">홈</button>
-            <button type="button">My Page</button>
-            <button type="button">채팅 목록</button>
+            <button type="button" onClick={handleHomeButtonClick}>
+              홈
+            </button>
+            <button type="button" onClick={handleMyPageButtonClick}>
+              My Page
+            </button>
+            <button type="button" onClick={handleChatButtonClick}>
+              채팅 목록
+            </button>
           </st.HomeAndMyAndChatContainer>
-          <st.PostButton type="button">동행 만들기</st.PostButton>
+          <st.PostButton type="button" onClick={handleCreatePostClick}>
+            동행 만들기
+          </st.PostButton>
         </>
       )}
     </st.MenuBarContainer>
